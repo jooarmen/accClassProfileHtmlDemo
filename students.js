43a268a2b36e4f7eb9a384c117ca93fc74eaaa83
jooarmen <126363143+jooarmen@users.noreply.github.com>
2023-03-16 19:02:30 -0700

    I have added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Jonathan',
+        lastName:'Armenta',
+        location: 'Santa Ana, CA',
+        html:'jonathanarmenta.html',
+        imagePath:'images/jonathanArmenta.jpg'
     },
 ]
 
